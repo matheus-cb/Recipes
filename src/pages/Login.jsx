5741e2f76a74305553133b42544757b4a1e46786
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-03-02 19:48:20 -0300

    Feat: req 04

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [buttonValid, setButtonValid] = useState(true);
+
+  function handleChange({ target: { name, value } }) {
+    setLogin({
+      ...login,
+      [name]: value,
+    });
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(login.email);
+    const num = 6;
+    const minCharacter = login.password.length >= num;
+
+    if (regex === true && minCharacter === true) {
+      setButtonValid(false);
+    } else {
+      setButtonValid(true);
+    }
+  }
+
   return (
     <form action="">
       <label htmlFor="">
         <input
           type="email"
+          name="email"
+          onChange={ handleChange }
           data-testid="email-input"
         />
       </label>
       <label htmlFor="">
         <input
           type="password"
+          name="password"
+          onChange={ handleChange }
           data-testid="password-input"
         />
       </label>
       <button
         data-testid="login-submit-btn"
+        disabled={ buttonValid }
       >
         Enter
       </button>
