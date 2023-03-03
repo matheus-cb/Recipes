2061259e2c831e26d728700369273a6b63159487
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-03-03 20:26:04 -0300

    Feat: req 09
    Adiciona a funcionalidade ao buttom de pesquisa. Cria testes para o componente Header com min de 90% de cobertura

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 
 export default function Header({ title, searchOn }) {
+  const [isInputVisible, setIsInputVisible] = useState(false);
   const history = useHistory();
+
+  const handleClickSearchButton = () => {
+    setIsInputVisible(!isInputVisible);
+  };
 
   const iconeDePesquisa = () => (
     <button
       data-testid="search-top-btn"
       src={ SearchIcon }
+      onClick={ handleClickSearchButton }
     >
-      <img src={ SearchIcon } alt="imagem de pesquisa" />
+      { isInputVisible ? <img src="https://cdn-icons-png.flaticon.com/512/7159/7159094.png" alt="imagem de um x" width="30" height="30" />
+        : <img src={ SearchIcon } alt="imagem de pesquisa" /> }
     </button>
   );
   return (
@@ -20,9 +27,11 @@ export default function Header({ title, searchOn }) {
       {
         searchOn ? iconeDePesquisa() : ''
       }
+
       <h1 data-testid="page-title">
         { title }
       </h1>
+
       <button
         data-testid="profile-top-btn"
         src={ ProfileIcon }
@@ -30,6 +39,18 @@ export default function Header({ title, searchOn }) {
       >
         <img src={ ProfileIcon } alt="imagem de perfil" />
       </button>
+
+      <hr />
+
+      {
+        isInputVisible ? (
+          <input
+            type="text"
+            data-testid="search-input"
+            placeholder="Digite sua pesquisa"
+          />
+        ) : ''
+      }
     </header>
   );
 }
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,9 +1,12 @@
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import Header from '../components/Header';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Componente Header', () => {
+  const SEARCH_INPUT = 'search-input';
+  const SEARCH_BTN = 'search-top-btn';
+
   test('renderiza os componentes no Header', () => {
     renderWithRouter(<Header title="title" searchOn />);
 
@@ -12,5 +15,54 @@ describe('Componente Header', () => {
     expect(title).toHaveTextContent('title');
     const buttons = screen.getAllByRole('button');
     expect(buttons).toHaveLength(2);
+  });
+
+  test('verifica se o botão de perfil redireciona para a página de perfil', () => {
+    const { history } = renderWithRouter(<Header title="title" searchOn />);
+
+    const profileButton = screen.getByTestId('profile-top-btn');
+    expect(profileButton).toBeInTheDocument();
+    userEvent.click(profileButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/profile');
+  });
+
+  test('verifica se o botão de pesquisa alterna a visibilidade do input de pesquisa', async () => {
+    // Esse código é assíncrono porque estamos usando o método findByTestId() para encontrar o input de pesquisa que deve estar visível após o clique no botão de pesquisa. findByTestId() é um método assíncrono que retorna uma promessa que é resolvida quando o elemento é encontrado. Portanto, precisamos usar o await para aguardar a promessa ser resolvida antes de prosseguir com o teste.
+    renderWithRouter(<Header title="title" searchOn />);
+
+    const searchButton = screen.getByTestId(SEARCH_BTN);
+    expect(searchButton).toBeInTheDocument();
+
+    const searchInput = screen.queryByTestId(SEARCH_INPUT);
+    expect(searchInput).not.toBeInTheDocument();
+
+    userEvent.click(searchButton);
+    const visibleSearchInput = await screen.findByTestId(SEARCH_INPUT);
+    expect(visibleSearchInput).toBeInTheDocument();
+
+    userEvent.click(searchButton);
+    const hiddenSearchInput = screen.queryByTestId(SEARCH_INPUT);
+    expect(hiddenSearchInput).not.toBeInTheDocument();
+  });
+
+  test('verifica se o icone de pesquisa é renderizado quando searchOn é verdadeiro', () => {
+    renderWithRouter(<Header title="title" searchOn />);
+
+    const searchButton = screen.getByTestId(SEARCH_BTN);
+    expect(searchButton).toBeInTheDocument();
+
+    const searchIcon = screen.queryByAltText('imagem de pesquisa');
+    expect(searchIcon).toBeInTheDocument();
+  });
+
+  test('verifica se nada é renderizado quando searchOn é falso', () => {
+    renderWithRouter(<Header title="title" searchOn={ false } />);
+
+    const searchButton = screen.queryByTestId(SEARCH_BTN);
+    expect(searchButton).not.toBeInTheDocument();
+
+    const searchIcon = screen.queryByAltText('imagem de pesquisa');
+    expect(searchIcon).not.toBeInTheDocument();
   });
 });
