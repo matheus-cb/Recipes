b65752061580b130796ea0e94006a7c92d3c1e1b
Levy Araujo <levy4raujo@gmail.com>
2023-03-10 11:24:39 -0300

    [25 Req] Em andamento ...
    Capturando as informações da API

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -10,26 +10,39 @@ export default function RecipeDetails(props) {
     },
   } = props;
 
-  const [card, setCard] = useState({});
+  const [photo, setPhoto] = useState('');
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [instruction, setInstruction] = useState('');
 
   useEffect(() => {
     async function getMeal() {
       const meal = await apiMealPerId(id);
-      setCard(meal);
+      setPhoto(meal.meals[0].strMealThumb);
+      setTitle(meal.meals[0].strMeal);
+      setCategory(meal.meals[0].strCategory);
+      setInstruction(meal.meals[0].strInstructions);
     }
     async function getDrink() {
       const drink = await apiDrinkPerId(id);
-      setCard(drink);
+      setPhoto(drink.drinks[0].strDrinkThumb);
+      setTitle(drink.drinks[0].strDrink);
+      setCategory(drink.drinks[0].strCategory);
+      setInstruction(drink.drinks[0].strInstructions);
     }
     if (url.includes('meal')) getMeal();
     if (url.includes('drink')) getDrink();
   }, [id, url]);
 
-  console.log(card);
   return (
     <div>
-      ReipeDetails
-      { id }
+      <h1>{ title }</h1>
+      <img
+        alt=""
+        src={ photo } // Recebe o link da Thumb, do Meals ou do Drink
+      />
+      <h2>{ category }</h2>
+      <p>{ instruction }</p>
     </div>
   );
 }
